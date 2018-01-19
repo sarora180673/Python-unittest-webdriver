1418271126477fec423b3fe53beb2117dda7e565
sarora180673 <suresh_arora@hotmail.com>
2018-01-19 18:32:51 +0530

    training scripts

--- Training/sample_script.py
+++ Training/sample_script.py
@@ -2,7 +2,9 @@ from selenium import webdriver
 
 driver = webdriver.Firefox()
 driver.get("http://www.google.com")
+driver.implicitly_wait(30)
 elem = driver.find_element_by_name("q")
 elem.send_keys("Hello WebDriver!")
 elem.submit()
 print(driver.title)
+driver.quit()
